723400aec164d8ad5758125f377aa3812acf1c3e
sahay-aaditya-raj <sahay.aaditya.raj@gmail.com>
2024-04-18 01:50:39 +0530

    init

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS  # Import CORS from flask_cors
 
-# Create Flask app
 app = Flask(__name__)
+CORS(app)  # Apply CORS to your Flask app
 
+# Sample endpoint to receive POST requests
+@app.route('/', methods=['POST'])
+def receive_data():
+    data = request.get_json()
+    print("Received Data:", data)
+    return jsonify({"message": "Data received successfully"}), 200
 
-
-# Define API endpoints
-@app.route('/api/', methods=['POST'])
-def api_test():
-    print(request.get_json())
-    return jsonify({"message":"received"})
-
-
-# Run the app
 if __name__ == '__main__':
     app.run(debug=True)
